f27b192d0aadbbc198418e81ab92acd3476b38d8
James <jdbor1@student.monash.edu>
2016-08-14 17:07:00 +1000

    First commit

---
+++ src/main.js
@@ -0,0 +1,23 @@
+
+var angular = require('angular');
+
+var ngflow = require('./lib/ng-flow-standalone.js')
+
+
+var app = angular.module('app', ['flow']);
+
+// Define the `PhoneListController` controller on the `phonecatApp` module
+// app.controller('PhoneListController', function PhoneListController($scope) {
+  // $scope.phones = [
+    // {
+      // name: 'Nexus S',
+      // snippet: 'Fast just got faster with Nexus S.'
+    // }, {
+      // name: 'Motorola XOOM™ with Wi-Fi',
+      // snippet: 'The Next, Next Generation tablet.'
+    // }, {
+      // name: 'MOTOROLA XOOM™',
+      // snippet: 'The Next, Next Generation tablet.'
+    // }
+  // ];
+// });
