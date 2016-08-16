29e3f69eec9046e6c2ebb8e08a21bd9f4f1c6d46
James <jdbor1@student.monash.edu>
2016-08-16 13:03:16 +1000

    small edits such as removing comments

--- src/main.js
+++ src/main.js
@@ -24,6 +24,7 @@ Dropzone.options.myDropzone = {
 			var canvas = document.getElementById('imageCanvas');
 			var ctx = canvas.getContext('2d');
 			var img = new Image();
+			img.src = window.URL.createObjectURL(file);
 			img.onload = function(){
 			  var ratio = img.width/img.height;
 			  if (img.width > img.height){
@@ -36,34 +37,8 @@ Dropzone.options.myDropzone = {
 			  ctx.drawImage(img,0,0,ctx.canvas.width,ctx.canvas.height);
 			  console.log("here")
 			}
-			img.src = window.URL.createObjectURL(file);
-			// var img = document.createElement("img");
-			// img.src = window.URL.createObjectURL(file);
-			// img.height = 80;
-			// // img.onload = function() {
-				// // // window.URL.revokeObjectURL(this.src);
-			// // }
-			
-			// var imgholder = document.getElementById("imgholder");
-			// imgholder.appendChild(img);
 			
 			
 		})
 	},
 }
-
-// Define the `PhoneListController` controller on the `phonecatApp` module
-// app.controller('PhoneListController', function PhoneListController($scope) {
-  // $scope.phones = [
-    // {
-      // name: 'Nexus S',
-      // snippet: 'Fast just got faster with Nexus S.'
-    // }, {
-      // name: 'Motorola XOOM™ with Wi-Fi',
-      // snippet: 'The Next, Next Generation tablet.'
-    // }, {
-      // name: 'MOTOROLA XOOM™',
-      // snippet: 'The Next, Next Generation tablet.'
-    // }
-  // ];
-// });
