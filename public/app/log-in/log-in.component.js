374fc48cad2ce3c8bc156fc5289a1e8fa404fb04
James <jdbor1@student.monash.edu>
2016-10-12 11:37:51 +1100

    angular googe log in bug fix 2

--- public/app/log-in/log-in.component.js
+++ public/app/log-in/log-in.component.js
@@ -1,21 +1,21 @@
 (function() {
 	  
 angular.
-  module('logIn').
-  component('logIn', {
-    templateUrl: 'app/log-in/log-in.template.html',
-    controller: ['GoogleSignin', function (GoogleSignin) {
-					var self = this;
-		
-						self.login = function () {
-							GoogleSignin.signIn()
-							.then(function (user) {
-								console.log(user);
-								
-							}, function (err) {
-								console.log(err);
-							});
-						};
-					}],
-  
+	module('logIn').
+	component('logIn', {
+		templateUrl: 'app/log-in/log-in.template.html',
+		controller: ['GoogleSignin', function (GoogleSignin) {
+						var self = this;
+			
+							self.login = function () {
+								GoogleSignin.signIn()
+								.then(function (user) {
+									console.log(user);
+									
+								}, function (err) {
+									console.log(err);
+								});
+							};
+						}],
+	}
 }());
