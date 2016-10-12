b1bddfeb1b873c1aafa18527da332cdc34434f2e
James <jdbor1@student.monash.edu>
2016-10-12 11:45:12 +1100

    added google log in client ID

--- public/app/log-in/log-in.config.js
+++ public/app/log-in/log-in.config.js
@@ -3,7 +3,7 @@
 angular.module('logIn')	
 	.config(['GoogleSigninProvider', function(GoogleSigninProvider) {
 		GoogleSigninProvider.init({
-			client_id: 'YOUR_CLIENT_ID',
+			client_id: '286208149051-71lk7sdm5plc2oih79p7mq8g80kla8te',
 		});
 	}]);
 	
