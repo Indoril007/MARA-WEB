881e3450e74f18b0ecaeafa914998b6c5c4baef7
James <jdbor1@student.monash.edu>
2016-10-12 11:10:00 +1100

    testing log in on server

--- public/app/log-in/log-in.component.js
+++ public/app/log-in/log-in.component.js
@@ -8,13 +8,13 @@ angular.
 		
 		var self = this;
 		
-		self.onSignIn = function(googleUser) {
-			  var profile = googleUser.getBasicProfile();
-			  console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-			  console.log('Name: ' + profile.getName());
-			  console.log('Image URL: ' + profile.getImageUrl());
-			  console.log('Email: ' + profile.getEmail());
-			}
+		// self.onSignIn = function(googleUser) {
+			  // var profile = googleUser.getBasicProfile();
+			  // console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+			  // console.log('Name: ' + profile.getName());
+			  // console.log('Image URL: ' + profile.getImageUrl());
+			  // console.log('Email: ' + profile.getEmail());
+			// }
 			
 	},
   });
