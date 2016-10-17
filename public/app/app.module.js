e030bb406bd759e3463b0901646d5981df8fb6f0
James <jdbor1@student.monash.edu>
2016-10-17 23:09:32 +1100

    removed angular material

--- public/app/app.module.js
+++ public/app/app.module.js
@@ -1,8 +1,7 @@
 (function(){
 	
 angular.module('mara-app', ['bp.img.cropper', 'navBar', 'dashboard', 'dragAndDrop', 'targetUpload', 
-							'augmentUpload', 'collection', 'logIn', 'google-signin', 'ngRoute',
-							'ngMaterial','ngMessages']
+							'augmentUpload', 'collection', 'logIn', 'google-signin', 'ngRoute']
 				);
 
 }());
